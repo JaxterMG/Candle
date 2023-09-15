fed47a59084d7f77d58b4eda167d6daa0102e7e2
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2023-09-15 20:46:54 +0300

    Added first interation scripts

---
+++ Assets/Scripts/Interations/IInteractable.cs
@@ -0,0 +1,11 @@
+namespace Interactions
+{
+    public interface IInteractive
+    {
+        public void Interact();
+    }
+    public interface IPickable
+    {
+        public void PickUp();
+    }
+}
