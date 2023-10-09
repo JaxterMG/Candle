2b11629ad5de524368a2fa2414e9f8f686b3ca3a
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2023-10-10 01:56:54 +0300

    Sound making particles

---
+++ Assets/ParticleSystemTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemTrigger : MonoBehaviour
+{
+    ParticleSystem _circleParticles;
+    void Start()
+    {
+        _circleParticles = GetComponent<ParticleSystem>();
+
+        InvokeRepeating("EmitParticles", 0, 2);
+    }
+
+    private void EmitParticles()
+    {
+        _circleParticles.Play();
+    }
+}
