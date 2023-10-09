e245cc3c4ee6ed3217ac3fad6bc059c041ca233d
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2023-10-10 00:25:28 +0300

    Door opening animation

--- Assets/Scripts/Interations/InteractiveObjects/Door.cs
+++ Assets/Scripts/Interations/InteractiveObjects/Door.cs
@@ -1,29 +1,39 @@
 using UnityEngine;
+using PrimeTween;
 
 namespace Interactions.InteractiveObjects
 {
     public class Door : MonoBehaviour, IInteractive, IHint
     {
         public string Name;
-        protected Transform _transform => transform;
+        protected Transform _transform;
         private bool _isOpened = false;
+        private Vector3 _startingRotation;
+        Tween _currentTween;
+
+        void Start()
+        {
+            _transform = transform;
+            _startingRotation = _transform.localEulerAngles;
+        }
 
         public void Interact()
         {
             Debug.Log($"Interacted with {Name}");
             float rotationValue = 0;
 
-            if(!_isOpened)
+            if (!_isOpened)
             {
-                rotationValue = _transform.rotation.y - 90;
+                rotationValue = _startingRotation.y -90;
             }
             else
             {
-                rotationValue = _transform.rotation.y + 90;
+                rotationValue = _startingRotation.y;
             }
-            
+
             _isOpened = !_isOpened;
-            _transform.Rotate(new Vector3(0, rotationValue, 0));
+            //_transform.Rotate(new Vector3(0, rotationValue,0));
+            Tween.Rotation(_transform, new Vector3(0, rotationValue, 0), 0.5f, Ease.Linear);
         }
         public string GetHint()
         {
